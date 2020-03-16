26f815eb595fd12e8ae098d6ca34aeb6614ffa73
mbilal044 <mbilalbutt003@gmail.com>
2020-03-16 09:44:01 +0500

    move banner code into banner const variable

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header";
 
+const banner = (
+  <div className="banner">
+    <div className="typewriter">
+      <h2>Welcome to my page!</h2>
+      <h1>
+        I am Kirill, <div id="stmt1">Acquistion strategist</div>
+        <div id="stmt2">Campaign executioner</div>
+        <div id="stmt3">Semi-technical marketing lead</div>
+      </h1>
+    </div>
+  </div>
+);
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="banner">
-        <div className="typewriter">
-          <h2>Welcome to my page!</h2>
-          <h1>
-            I am Kirill, <div id="stmt1">Acquistion strategist</div>
-            <div id="stmt2">Campaign executioner</div>
-            <div id="stmt3">Semi-technical marketing lead</div>
-          </h1>
-        </div>
-      </div>
+      {banner}
     </div>
   );
 }
