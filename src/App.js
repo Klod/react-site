21fbc1ac5923cab45cebc132b39fbd6206162934
mbilal044 <mbilalbutt003@gmail.com>
2020-03-16 09:38:31 +0500

    banner with typewriter animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import Header from "./components/header";
 
@@ -7,6 +7,16 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="banner">
+        <div className="typewriter">
+          <h2>Welcome to my page!</h2>
+          <h1>
+            I am Kirill, <div id="stmt1">Acquistion strategist</div>
+            <div id="stmt2">Campaign executioner</div>
+            <div id="stmt3">Semi-technical marketing lead</div>
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
